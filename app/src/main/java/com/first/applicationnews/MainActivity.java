080aa69e5ea2a06daa5de2d898b80f6f694eb4c4
ksenia <ksy184694@gmail.com>
2024-06-22 15:40:13 +0300

    init commit

---
+++ app/src/main/java/com/first/applicationnews/MainActivity.java
@@ -0,0 +1,24 @@
+package com.first.applicationnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Новости науки");
+    }
+}
